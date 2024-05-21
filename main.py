a2232997cae82cfd8bef7e2000cd126e5931f5c1
unknown <marko.lopicic995@gmail.com>
2024-05-21 03:04:41 +0200

    not initial

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from trelloapp.trello_app import TrelloApp
-#from dotenv import load_dotenv
-#import os
 
-#load_dotenv()
 
-#API_KEY = os.getenv('API_KEY')
-#API_TOKEN = os.getenv('API_TOKEN')
-#BOARD_ID = os.getenv('BOARD_ID')
+
+#KAKO SAKRITI OVE PODATKE?
+
 
 API_KEY='882a3299e3be109c4baeca86bd7f9f5e'
 API_TOKEN='ATTAe2339fc18de1cd1f7b71dd3705610cf92a5e83c55da4928bc74fd6c0d216fdee4D596758'
 BOARD_ID='6643da406a3e2b29e3472a90'
+
+
+#KAKO SAKRITI OVE PODATKE?
     
 
 def main():
